d03d3ab1df8cba5925a512fe31c1a80530414204
Alexia Kattah <alexia.kattah@oqvocequer.com.br>
2023-01-31 20:39:52 -0300

    iniciando projeto na aula EBAC

--- src/server.js
+++ src/server.js
@@ -8,17 +8,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/users', userRoutes);
-
-app.use((err, request, response, next) => {
-  if (err instanceof Error) {
-    return response.status(400).json({
-      message: err.message,
-    });
-  }
-  return response.status(500).json({
-    status: '500',
-    message: 'Internal server error',
-  });
-});
+app.use('/foods', foodsRoutes);
 
 app.listen(process.env.port || 3333, () => console.log('Server is running '));
